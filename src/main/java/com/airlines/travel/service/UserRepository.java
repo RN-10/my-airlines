33cf4204d85c63ec2cc7b4d89b1b6e828b0ecc19
RN-10 <40533096+RN-10@users.noreply.github.com>
2019-12-17 18:29:20 +0530

    Update UserRepository.java

--- src/main/java/com/airlines/travel/service/UserRepository.java
+++ src/main/java/com/airlines/travel/service/UserRepository.java
@@ -1,7 +1,5 @@
 package com.airlines.travel.service;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.airlines.travel.Entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
-	Optional<User> findByName(String name);
+	
 }
