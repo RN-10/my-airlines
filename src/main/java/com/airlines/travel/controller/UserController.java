750584e2bc5165a94a2c470d57f0ca3766f2fa09
RN-10 <40533096+RN-10@users.noreply.github.com>
2019-12-17 16:27:49 +0530

    Update UserController.java

--- src/main/java/com/airlines/travel/controller/UserController.java
+++ src/main/java/com/airlines/travel/controller/UserController.java
@@ -48,21 +48,6 @@ public class UserController {
 		return userService.getUser(userId);
 	}
 	
-	@RequestMapping(value="/authenticate", method=RequestMethod.POST)
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-		try {
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-				);
-		} catch (BadCredentialsException e) {
-				throw new Exception("Incorrect Username or Password", e);
-		   }
-		final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-		final String jwt = jwtUtil.generateToken(userDetails);
-		
-		return ResponseEntity.ok(new AuthenticationResponse(jwt));
-	}
-	
 	@RequestMapping(value="/user", method=RequestMethod.POST)
 	public void addUser(@RequestBody User user) {
 		userService.addUser(user);
